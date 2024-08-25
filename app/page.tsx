b12b326074b8159aa9e0127690ac1d4d48276c64
LUCUSZ <bigcsaharat@gmail.com>
2024-08-26 02:17:47 +0700

    Fontend/Big

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,23 @@
-"use client"
+"use client";
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import ChoiceBox from './components/choiceBox'; 
-import { Choice } from './components/choiceBox';
-import { FaPlay, FaPause } from 'react-icons/fa'; // Import play and pause icons
+import { FaPlay, FaPause } from 'react-icons/fa';
+import { Caveat } from '@next/font/google';
+import { Sriracha } from '@next/font/google';
 
-// Define the type for personality roles
-type PersonalityType = 'Parrot' | 'Captain' | 'Doctor' | 'Chef' | 'TikToker' | 'SleepyCat' | 'Cannon' | 'Internship' | 'Traveler' | 'Map' | 'Mermaid';
-
-// Define the type for the score object
-type Score = {
-  [key in PersonalityType]?: number;
-};
-
-interface TextChoice {
-  type: "text";
-  label: string;
-  score: Score;
-  sound?: string;
-}
-
-interface ImageChoice {
-  type: "image";
-  src: string;
-  alt: string;
-  score: Score;
-  sound?: string;
-}
+const caveat = Caveat({ subsets: ['latin'], weight: ['400', '700'] });
+const sriracha = Sriracha({ subsets: ['thai'], weight: '400' });
 
 export default function Home() {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [personalityScores, setPersonalityScores] = useState<Score>({
-    Parrot: 0,
-    Captain: 0,
-    Doctor: 0,
-    Chef: 0,
-    TikToker: 0,
-    SleepyCat: 0,
-    Cannon: 0,
-    Internship: 0,
-    Traveler: 0,
-    Map: 0,
-    Mermaid: 0,
-  });
-
   const router = useRouter();
   const [backgroundAudio, setBackgroundAudio] = useState<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [specialSelection, setSpecialSelection] = useState(false);
+  const [day, setDay] = useState<string>('');
+  const [month, setMonth] = useState<string>('');
+  const [year, setYear] = useState<string>('');
+  const [isStartPage, setIsStartPage] = useState(true);
+  const [isInputPage, setIsInputPage] = useState(false);
+  const [isResultPage, setIsResultPage] = useState(false);
 
   useEffect(() => {
     const audio = new Audio('/musics/Dawn by Sappheiros.mp3');
@@ -63,8 +33,10 @@ export default function Home() {
     });
 
     return () => {
-      audio.pause();
-      audio.currentTime = 0;
+      if (backgroundAudio) {
+        backgroundAudio.pause();
+        backgroundAudio.currentTime = 0;
+      }
     };
   }, []);
 
@@ -81,158 +53,127 @@ export default function Home() {
     }
   };
 
-  const [transitionActive, setTransitionActive] = useState(false);
-
-  const handleChoiceClick = (choice: Choice) => {
-    if (choice.type === "text" && choice.label === "เซ็กซี่แม่เสือสาว") {
-      setSpecialSelection(true);
-    }
-
-    setPersonalityScores((prevScores) => {
-      const newScores = { ...prevScores };
-      for (const [dimension, points] of Object.entries(choice.score)) {
-        // Type assertion to ensure that dimension is a valid key
-        const key = dimension as PersonalityType;
-        if (newScores.hasOwnProperty(key)) {
-          newScores[key] = (newScores[key] || 0) + (points || 0);
-        }
-      }
-      return newScores;
-    });
-
-    if (currentQuestion < questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      setTransitionActive(true);
-
-      setTimeout(() => {
-        if (specialSelection) {
-          router.push(`/result?type=fixed&sound=secretSong`);
-        } else {
-          const maxScore = Math.max(...Object.values(personalityScores));
-          const result = Object.keys(personalityScores).find((key): key is PersonalityType => personalityScores[key as PersonalityType] === maxScore) as PersonalityType;
-          router.push(`/result?type=${result}&sound=${choice.sound}`);
-        }
-      }, 2000); // Wait for the transition to complete before navigating
-    }
+  const renderMonthOptions = () => {
+    const months = [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    return months.map((month, index) => (
+      <option key={index + 1} value={index + 1}>
+        {month}
+      </option>
+    ));
   };
 
-  const questions: { title: string; choices: Choice[] }[] = [
-    {
-      title: "How do you usually approach challenges?",
-      choices: [
-        { type: "text", label: "I face them head-on with confidence.", score: { Captain: 1 } },
-        { type: "text", label: "I look for creative solutions.", score: { TikToker: 1, Parrot: 1 } },
-        { type: "text", label: "I try to help others through the challenge.", score: { Doctor: 1 } },
-        { type: "text", label: "I prefer to avoid conflict when I can.", score: { SleepyCat: 1 } },
-        { type: "text", label: "I take a moment to reflect before acting.", score: { Traveler: 1, Map: 1 } },
-      ],
-    },
-    {
-      title: "What’s your role in a group project?",
-      choices: [
-        { type: "text", label: "I lead and make decisions.", score: { Captain: 1 } },
-        { type: "text", label: "I come up with the most creative ideas.", score: { TikToker: 1, Parrot: 1 } },
-        { type: "text", label: "I support and ensure everyone feels heard.", score: { Doctor: 1 } },
-        { type: "text", label: "I handle my part diligently.", score: { Internship: 1 } },
-        { type: "text", label: "I observe and contribute when necessary.", score: { Internship: 1 } },
-      ],
-    },
-    {
-      title: "What do you enjoy most about traveling?",
-      choices: [
-        { type: "text", label: "Discovering new places and people.", score: { Traveler: 1 } },
-        { type: "text", label: "Documenting and sharing my experiences.", score: { TikToker: 1, Parrot: 1 } },
-        { type: "text", label: "Helping others along the way.", score: { Doctor: 1 } },
-        { type: "text", label: "Finding time to relax and enjoy the moment.", score: { SleepyCat: 1 } },
-        { type: "text", label: "Gathering souvenirs and memories for reflection.", score: { Internship: 1 } },
-      ],
-    },
-    {
-      title: "What’s your favorite place on a ship?",
-      choices: [
-        { type: "text", label: "The captain's quarters.", score: { Captain: 1 } },
-        { type: "text", label: "The kitchen, where food is always available.", score: { Chef: 1 } },
-        { type: "text", label: "The infirmary where I can help others.", score: { Doctor: 1 } },
-        { type: "text", label: "The deck with a beautiful view.", score: { Traveler: 1 } },
-        { type: "text", label: "The hammock for a good nap.", score: { SleepyCat: 1, Mermaid: 1 } },
-      ],
-    },
-    {
-      title: "Which of these best describes your dream adventure?",
-      choices: [
-        { type: "text", label: "Sailing the seas as a leader.", score: { Captain: 1 } },
-        { type: "text", label: "Creating a travel vlog or content.", score: { TikToker: 1, Parrot: 1 } },
-        { type: "text", label: "Helping those in need on the journey.", score: { Doctor: 1 } },
-        { type: "text", label: "A relaxing vacation with great food.", score: { Chef: 1 } },
-        { type: "text", label: "A journey of self-discovery.", score: { Traveler: 1 } },
-        { type: "text", label: "Exploring unknown territories with a map.", score: { Map: 1 } },
-      ],
-    },
-    {
-      title: "What would you do if your friend was feeling down?",
-      choices: [
-        { type: "text", label: "Motivate them to keep going.", score: { Captain: 1 } },
-        { type: "text", label: "Cheer them up with a funny story.", score: { Parrot: 1, Mermaid: 1 } },
-        { type: "text", label: "Take care of them and listen to their problems.", score: { Doctor: 1 } },
-        { type: "text", label: "Offer them food or a treat to feel better.", score: { Chef: 1 } },
-        { type: "text", label: "Encourage them to take a break and rest.", score: { SleepyCat: 1, Map: 1 } },
-      ],
-    },
-    {
-      title: "How do you spend your free time?",
-      choices: [
-        { type: "text", label: "Organizing or planning something.", score: { Captain: 1, Map: 1 } },
-        { type: "text", label: "Making videos or taking pictures.", score: { TikToker: 1, Parrot: 1 } },
-        { type: "text", label: "Helping someone with their tasks.", score: { Doctor: 1 } },
-        { type: "text", label: "Napping or taking it easy.", score: { SleepyCat: 1 } },
-        { type: "text", label: "Reading or journaling.", score: { Traveler: 1 } },
-      ],
-    },
-    {
-      title: "How do you react to a crisis?",
-      choices: [
-        { type: "text", label: "I take charge and give orders.", score: { Captain: 1 } },
-        { type: "text", label: "I document what's happening.", score: { TikToker: 1 } },
-        { type: "text", label: "I help people stay calm and offer support.", score: { Doctor: 1 } },
-        { type: "text", label: "I focus on staying safe and comfortable.", score: { SleepyCat: 1 } },
-        { type: "text", label: "I make notes and figure out a plan.", score: { Map: 1 } },
-      ],
-    },
-    {
-      title: "What’s your favorite ship activity?",
-      choices: [
-        { type: "text", label: "Directing the crew and navigating.", score: { Captain: 1 } },
-        { type: "text", label: "Creating engaging content for the voyage.", score: { TikToker: 1 } },
-        { type: "text", label: "Helping others with health and wellness.", score: { Doctor: 1 } },
-        { type: "text", label: "Cooking and serving delicious meals.", score: { Chef: 1 } },
-        { type: "text", label: "Relaxing and enjoying the view.", score: { SleepyCat: 1 } },
-      ],
-    },
-  ];
+  const renderYearOptions = () => {
+    const currentYear = new Date().getFullYear();
+    const years = [];
+    for (let i = currentYear; i >= 1900; i--) {
+      years.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
+    }
+    return years;
+  };
+
+  const renderDayOptions = () => {
+    return Array.from({ length: 31 }, (_, i) => (
+      <option key={i + 1} value={i + 1}>
+        {i + 1}
+      </option>
+    ));
+  };
 
   return (
     <div>
       <div className="flex justify-center bg-white h-screen">
-      <div
-        className="relative flex justify-center items-center bg-cover bg-center w-full max-w-md h-full"
-        style={{ backgroundImage: "url('/images/GDSC_BGSea.jpg')" }}
-      >
-          <ChoiceBox
-              key={currentQuestion}
-              title={questions[currentQuestion].title} // Add the question title here
-              choices={questions[currentQuestion].choices} // Pass the entire array of choices
-              onChoiceClick={(choice) => handleChoiceClick(choice)}
-          />              
-        <button 
-          onClick={handlePlayMusic}
-          className="top-4 right-4 absolute bg-purple-600 p-2 rounded-full text-white"
-        >
-          {isPlaying ? <FaPause /> : <FaPlay />}
-        </button>
-        <div className={`fade-to-black ${transitionActive ? 'active' : ''}`}></div>
-        
-      </div>
+        {isStartPage && (
+          <div className="relative flex justify-center items-center bg-cover bg-center w-full max-w-md h-full"
+            style={{ backgroundImage: "url('/images/GDSC_BGSea.jpg')" }}>
+            <div className="bg-white bg-opacity-80 shadow-md p-6 rounded-xl w-3/4 max-w-md h-[500px]">
+              <div className="justify-center">
+                <div className="flex justify-center">
+                  <h1 className={`mt-8 font-bold text-4xl ${caveat.className}`}>GDSC Horoscope</h1>
+                </div>
+                <ul className={`mt-8 font-light text-[16px] ${sriracha.className}`}>
+                  <li>อย่าจริงจังเกินไป: ราศีเป็นการทำนายโดยรวม ไม่สามารถบอกถึงรายละเอียดในชีวิตของแต่ละบุคคลได้อย่างแม่นยำ</li>
+                  <li>ใช้เป็นแนวทาง ไม่ใช่กฎเกณฑ์: ราศีสามารถให้แนวคิดและมุมมองใหม่ๆ แต่ไม่ควรกำหนดชีวิตของคุณ</li>
+                  <li>เชื่อสัญชาตญาณของตัวเอง: สัญชาตญาณและความรู้สึกของคุณเองมักจะเป็นตัวบอกทางที่ดีที่สุด</li>
+                </ul>
+              </div>
+              <button
+                onClick={handlePlayMusic}
+                className="top-4 right-4 absolute bg-purple-600 p-2 rounded-full text-white"
+              >
+                {isPlaying ? <FaPause /> : <FaPlay />}
+              </button>
+              <div className="flex justify-center py-6">
+                <button
+                  onClick={() => { setIsStartPage(false); setIsInputPage(true); }} 
+                  className={`bg-sky-950 px-6 py-3 rounded-full text-3xl text-white ${caveat.className}`}
+                >
+                  Start Quiz
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        {isInputPage && (
+          <div className="relative flex justify-center items-center bg-cover bg-center w-full max-w-md h-full"
+            style={{ backgroundImage: "url('/images/GDSC_BGSea.jpg')" }}>
+            <div className="bg-white bg-opacity-80 shadow-md p-6 rounded-xl w-3/4 max-w-md h-[500px]">
+              <div className="flex justify-center">
+                <h1 className={`mt-4 font-bold text-4xl ${caveat.className}`}>Daily Horoscope</h1>
+              </div>
+              <h1 className={`flex justify-center mt-6 font-normal text-[30px] ${caveat.className}`}>Enter your Date of Birth</h1>
+              <div className="flex justify-center mt-4">
+                <select className={`border-2 bg-gray-300 px-2 py-2 border-black rounded-lg text-lg ${caveat.className}`}
+                  id="day" value={day} onChange={(e) => setDay(e.target.value)}>
+                  <option value="">Day</option>
+                  {renderDayOptions()}
+                </select>
+                <select className={`mx-3 border-2 bg-gray-300 px-4 py-2 border-black rounded-lg text-lg ${caveat.className}`}
+                  id="month" value={month} onChange={(e) => setMonth(e.target.value)}>
+                  <option value="">Month</option>
+                  {renderMonthOptions()}
+                </select>
+                <select className={`border-2 bg-gray-300 px-4 py-2 border-black rounded-lg text-lg ${caveat.className}`}
+                  id="year" value={year} onChange={(e) => setYear(e.target.value)}>
+                  <option value="">Year</option>
+                  {renderYearOptions()}
+                </select>
+              </div>
+              <h1 className={`flex justify-center mt-6 font-normal text-[30px]`}>เทส</h1>
+              <div className="flex justify-center py-6">
+                <button
+                  onClick={() => { setIsInputPage(false); setIsResultPage(true); }} 
+                  className={`bg-sky-950 px-6 py-3 rounded-full text-3xl text-white ${caveat.className}`}
+                >
+                  Go
+                </button>
+              </div>
+              <button
+                onClick={handlePlayMusic}
+                className="top-4 right-4 absolute bg-purple-600 p-2 rounded-full text-white"
+              >
+                {isPlaying ? <FaPause /> : <FaPlay />}
+              </button>
+            </div>
+          </div>
+        )}
+        {isResultPage && (
+          // Define Result Page Content
+          <div className="relative flex justify-center items-center bg-cover bg-center w-full max-w-md h-full"
+            style={{ backgroundImage: "url('/images/GDSC_BGSea.jpg')" }}>
+            <div className="bg-white bg-opacity-80 shadow-md p-6 rounded-xl w-3/4 max-w-md h-auto">
+            <div className="flex justify-center">
+                <h1 className={`mt-2 font-bold text-4xl ${caveat.className}`}>Your Fortune</h1>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
